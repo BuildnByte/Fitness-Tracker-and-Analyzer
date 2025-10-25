7ad4ec6720bc8fa5cb0a32b8a437b16de47af56d
Yash Kasle <yashkasle@gmail.com>
2025-10-25 16:37:47 +0530

    corrected build_files.sh file

--- fitness_Tracker_and_Adviser/build_files.sh
+++ fitness_Tracker_and_Adviser/build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Run collectstatic
 python3.12 manage.py collectstatic --noinput --clear
